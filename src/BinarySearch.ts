1497f7a5f8175d481d1056b83e65ff7c8f88f301
David Valor <david.valor@geoselfie.com.au>
2021-10-08 13:34:42 -0400

    Tests with typescript

---
+++ src/BinarySearch.ts
@@ -0,0 +1,11 @@
+/**
+ * @name 704. Binary Search
+ * @description Make a binary search for getting a parameter of an array
+ * @author davidvalorwork@gmail.com
+ * @param nums Array of number for search
+ * @param target What I need to found
+ */
+function search(nums: number[], target: number): number {
+
+  return 0
+}
